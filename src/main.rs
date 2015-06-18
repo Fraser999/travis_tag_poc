96bf9d5cf2821b72ff46fd778b2da756791a34fb
Krishna <krishnaof1988@gmail.com>
2015-06-18 22:26:24 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
-/// Returns version number 0.2
+/// Returns version number 0.2.1
 pub fn get_version() -> String {
-    "version 0.2".to_string()
+    "version 0.2.1".to_string()
 }
 
 fn main() {
